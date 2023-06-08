9cc3dbd3f466e0e71c46977f71d24e49f0ab0510
ST10279733 <sikblessing4@ST10249733.com>
2023-06-08 22:14:48 +0200

    Second Commit

---
+++ src/main/java/com/mycompany/arrayint2/ArrayInt2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrayint2;
+
+
+public class ArrayInt2 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3, 4, 5};
+        
+        //using a for loop in an array 
+        for (int i = 0; 1 < numbers.length; i++){
+           System.out.println("Element at index: " +numbers[i]);
+        }
+    }
+}
